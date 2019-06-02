21f6f3fcdb56be16b5417fe6ac1be370120997f6
Hasanain Jamal <hasanain.jm@gmail.com>
2019-06-01 19:49:38 -0700

    cleans up election display and fixes multiple candidate same vote issue

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (Model, Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, li, text)
+import Html exposing (Html, button, div, h2, h3, li, text)
 import Html.Events exposing (onClick)
 
 
@@ -26,12 +26,13 @@ init =
     , ballots =
         [ [ "Tony Stark", "John Wick" ]
         , [ "Wally West", "Steve Rogers", "Tony Stark", "John Wick" ]
-        , [ "Steve Rogers", "Wally West", "Tony Stark" ]
+        , [ "Steve Rogers", "Wally West", "John Wick" ]
         , [ "Tony Stark", "John Wick", "Steve Rogers" ]
         , [ "John Wick" ]
         , [ "Tony Stark" ]
         , [ "John Wick", "Tony Stark" ]
         , [ "John Wick", "Tony Stark" ]
+        , [ "Wally West" ]
         ]
     }
 
@@ -57,16 +58,17 @@ tallyVotes c v p =
 -- else run another round of first past the post with
 --  everyone except the candidate with the least vote
 --  each candidate who voted for the least person's second choice
+-- TODO: what happens when you have every candidate has the same amount of votes
 
 
-runInstantRunoffElection : Model -> List ( String, Int )
+runInstantRunoffElection : Model -> List (List ( String, Int ))
 runInstantRunoffElection ({ candidates, ballots } as election) =
     let
         roundResult =
-            runFPTPElection { candidates = candidates, ballots = ballots }
+            runFPTPElection election
 
         hasMajority =
-            List.any (\( _, i ) -> i >= List.length ballots // 2) roundResult
+            List.any (\( _, v ) -> v > List.length ballots // 2) roundResult
 
         singleCandidateLeft =
             List.length roundResult == 1
@@ -86,14 +88,17 @@ runInstantRunoffElection ({ candidates, ballots } as election) =
         candidatesWithLeastVotes =
             List.map (\( c, _ ) -> c) <|
                 List.filter
-                    (\( c, i ) -> i == leastVotes)
+                    (\( c, v ) -> v == leastVotes)
                     roundResult
+
+        everyoneHasTheSameVotes =
+            List.length candidatesWithLeastVotes == List.length roundResult
     in
-    if hasMajority || singleCandidateLeft || noCandidatesLeft then
-        roundResult
+    if hasMajority || singleCandidateLeft || noCandidatesLeft || everyoneHasTheSameVotes then
+        [ roundResult ]
 
     else
-        runInstantRunoffElection <| removeCandidates election candidatesWithLeastVotes
+        roundResult :: (runInstantRunoffElection <| removeCandidates election candidatesWithLeastVotes)
 
 
 removeCandidates : Model -> List String -> Model
@@ -124,9 +129,6 @@ removeCandidate model candidate =
 runFPTPElection : Model -> List ( String, Int )
 runFPTPElection { candidates, ballots } =
     let
-        initialCount =
-            List.map (\n -> ( n, 0 )) candidates
-
         firstVotes =
             List.map
                 (\b ->
@@ -134,7 +136,7 @@ runFPTPElection { candidates, ballots } =
                         v :: _ ->
                             Just v
 
-                        _ ->
+                        [] ->
                             Nothing
                 )
                 ballots
@@ -144,7 +146,7 @@ runFPTPElection { candidates, ballots } =
                 (\c -> ( c, List.foldl (tallyVotes c) 0 firstVotes ))
                 candidates
     in
-    List.reverse <| List.sortBy (\( k, v ) -> v) tally
+    List.reverse <| List.sortBy (\( _, v ) -> v) tally
 
 
 type Msg
@@ -168,10 +170,41 @@ rankedCandidatesView : Model -> List (Html Msg)
 rankedCandidatesView model =
     let
         electionResult =
-            runInstantRunoffElection model
+            runFPTPElection model
     in
     List.map candidateView electionResult
 
 
+instantRunoffView : Model -> List (Html Msg)
+instantRunoffView model =
+    let
+        electionResults =
+            List.reverse <| runInstantRunoffElection model
+
+        labels =
+            List.indexedMap
+                (\i _ ->
+                    if i == 0 then
+                        h3 [] [ text "Final Round (Result)" ]
+
+                    else
+                        h3 [] [ text <| "Round " ++ String.fromInt (List.length electionResults - i) ]
+                )
+                electionResults
+
+        resultsView =
+            List.map (List.map candidateView) electionResults
+
+        resultsWithLabels =
+            List.map2 (\l r -> l :: r) labels resultsView
+    in
+    List.concatMap (\x -> x) resultsWithLabels
+
+
 view model =
-    div [] (rankedCandidatesView model)
+    div []
+        [ h2 [] [ text "First Past the post" ]
+        , div [] (rankedCandidatesView model)
+        , h2 [] [ text "Instant Runoff" ]
+        , div [] (instantRunoffView model)
+        ]
